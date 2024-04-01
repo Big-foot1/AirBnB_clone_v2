63cf1ddabdf49ef9a5e2cc29c48438d7a2d2d468
Big-foot1 <imutyaba11@gmail.com>
2024-04-01 15:28:56 -0700

    changes task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,19 +16,17 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     try:
-        arch_name = archive_path.split('/')[1]
-        arch_name_nex = arch_name.split(".")[0]
-        re_path = "/data/web_static/releases/" + arch_name_nex
-        up_path = '/tmp/' + arch_name
-        put(archive_path, up_path)
-        run('mkdir -p ' + re_path)
-        run('tar -xzf /tmp/{} -C {}/'.format(arch_name, re_path))
-        run('rm {}'.format(up_path))
-        mv = 'mv ' + re_path + '/web_static/* ' + re_path + '/'
-        run(mv)
-        run('rm -rf ' + re_path + '/web_static')
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s ' + re_path + ' /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
